084fd9242d12088c5d9c2dde6046cbc8b0e24fca
reepat2023 <reepatel2023@gmail.com>
2023-08-09 04:27:51 +0530

    Initial commit

---
+++ src/main/java/com/gorest/constants/EndPoints.java
@@ -0,0 +1,14 @@
+package com.gorest.constants;
+
+public class EndPoints {
+    /**
+     * This is Endpoints of student api
+     */
+    public static final String USERS_BYID = "/{userId}";
+
+
+    /**
+     * This is Endpoints of Authentication api
+     */
+
+}
